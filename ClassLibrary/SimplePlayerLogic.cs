caa401f50ff9edb9c406e574bf2e74bd4314a691
dark567 <darkmit@list.ru>
2019-09-07 12:34:11 +0300

    201909071235

---
+++ ClassLibrary/SimplePlayerLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class SimplePlayerLogic
+    {
+        public static void CalcSimplePlayer(object obj)
+        {
+            Random ran = new Random();
+            int SimbleBehavior;
+
+            for (int i = 0; i < 100; i++)
+            {
+                SimbleBehavior = ran.Next(40, 140);
+                if (Convert.ToInt32(obj) == SimbleBehavior)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Lucky[Step:{i}] {SimbleBehavior} = {Convert.ToInt32(obj)}");
+                    Console.ResetColor();
+                    break;
+                }
+                else Console.WriteLine($"( {SimbleBehavior} != {Convert.ToInt32(obj)}");
+            }
+        }
+
+        public static void CalcNotepadPlayer(object obj)
+        {
+            Random ran = new Random();
+            int SimbleBehavior;
+
+            for (int i = 0; i < 100; i++)
+            {
+                SimbleBehavior = ran.Next(40, 140);
+                if (Convert.ToInt32(obj) == SimbleBehavior)
+                {
+                    SimplePlayerStep = 
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Lucky[Step:{i}] {SimbleBehavior} = {Convert.ToInt32(obj)}");
+                    Console.ResetColor();
+                    break;
+                }
+                else Console.WriteLine($"( {SimbleBehavior} != {Convert.ToInt32(obj)}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    public enum Players
+    {
+        SimplePlayer,
+        NotepadPlayer
+    }
+    class Program
+    {
+        public static int MainSize, SimplePlayerStep, NotepadPlayerStep;
+        
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            MainSize = ran.Next(40, 140);
+            string typePlayer;
+            //int[] Size = new 
+
+            Thread simplePlayer = new Thread(new ParameterizedThreadStart(SimplePlayerLogic.CalcSimplePlayer));
+            simplePlayer.Start(MainSize);
+
+            Thread notepadPlayer = new Thread(new ParameterizedThreadStart(SimplePlayerLogic.CalcNotepadPlayer));
+            simplePlayer.Start(MainSize);
+
+            Console.WriteLine($"{MainSize}");
+            Console.ReadLine();
+        }
+
+       
+    }
+}
